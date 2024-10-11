21e82e5fb5a342851ebea3e4b1d304668cd2006f
fanoflix <majidammar428@gmail.com>
2024-10-11 17:10:39 +0500

    Modiefied base page a bit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,56 +1,28 @@
 "use client";
-import { useTheme } from "next-themes";
-import Image from "next/image";
+import MyLogo from "../components/MyLogo/MyLogo";
 import TooltipWrapper from "../components/Tooltip/TooltipWrapper";
 
 export default function Home() {
-  const { setTheme } = useTheme();
-
   return (
-    <div className="flex flex-col gap-4 items-center justify-center">
+    <div className="flex flex-col gap-24 items-center justify-center">
       <h1 className="text-4xl font-bold">Heading</h1>
       <p>Paragraph</p>
-      <button
-        onClick={() => {
-          setTheme("dark");
-        }}
-        className="bg-blue-500 text-white px-4 py-2 rounded-md"
-      >
-        Dark
-      </button>
-      <button
-        onClick={() => {
-          setTheme("light");
-        }}
-        className="bg-blue-500 text-white px-4 py-2 rounded-md"
-      >
-        Light
-      </button>
-      <button
-        onClick={() => {
-          setTheme("system");
-        }}
-        className="bg-blue-500 text-white px-4 py-2 rounded-md"
-      >
-        System
-      </button>
 
       <TooltipWrapper
         tooltipContent="World of Warcraft"
         side="bottom"
         sideOffset={20}
       >
-        <div className="flex items-center gap-2 cursor-pointer text-x">
-          <Image
-            className="cursor-pointer"
-            src="/logo2.svg"
-            alt="Logo"
-            width={32}
-            height={32}
-          />{" "}
-          Test
+        <div className="flex items-center gap-2 cursor-pointer text-x bg-gray-700 p-2">
+          Hover me
         </div>
       </TooltipWrapper>
+
+      <div className="flex gap-2 items-center justify-center"></div>
+
+      <div>
+        <MyLogo />
+      </div>
     </div>
   );
 }
