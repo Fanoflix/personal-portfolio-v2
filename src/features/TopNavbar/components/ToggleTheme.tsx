1e76e9912941c5421dc3bdc342b42e260cad2d6d
fanoflix <majidammar428@gmail.com>
2024-10-13 13:33:20 +0500

    DEV: Fixed ToggleTheme component's animation

--- src/features/TopNavbar/components/ToggleTheme.tsx
+++ src/features/TopNavbar/components/ToggleTheme.tsx
@@ -1,3 +1,4 @@
+import useToggle from "@/src/hooks/useToggle";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import { useEffect, useState } from "react";
@@ -6,31 +7,26 @@ import { iconHeightWidth } from "../constants";
 export default function ToggleTheme() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
-  const [isRotating, setIsRotating] = useState(false);
+  const [isRotating, toggleRotation] = useToggle(false);
 
   useEffect(() => {
     setMounted(true);
   }, []);
 
   const toggleTheme = () => {
-    setIsRotating(true);
+    toggleRotation();
     setTheme(theme === "light" ? "dark" : "light");
-    setTimeout(() => setIsRotating(false), 250); // Reset after animation
   };
 
   if (!mounted) return null;
 
-  const containerStyle = {
-    cursor: "pointer",
-  };
-
   const iconWrapperStyle = {
-    transition: "transform 500ms ease",
-    transform: isRotating ? "rotateZ(360deg)" : "rotateZ(-360deg)",
+    transition: "transform 350ms ease",
+    transform: isRotating ? "rotateZ(360deg)" : "rotateZ(0deg)",
   };
 
   return (
-    <div onClick={toggleTheme} style={containerStyle}>
+    <button className="flex" onClick={toggleTheme}>
       <div style={iconWrapperStyle}>
         {theme === "light" ? (
           <Image
@@ -48,6 +44,6 @@ export default function ToggleTheme() {
           />
         )}
       </div>
-    </div>
+    </button>
   );
 }
