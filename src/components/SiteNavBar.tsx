2c362466d188ef19f3f3977615b15ac8ad1876aa
fanoflix <majidammar428@gmail.com>
2024-10-15 17:15:15 +0500

    DEV: Added prefetch to nav links in SiteNavBar

--- src/components/SiteNavBar.tsx
+++ src/components/SiteNavBar.tsx
@@ -5,10 +5,14 @@ export default function SiteNavbar() {
   return (
     <nav className="flex items-end gap-5 justify-end">
       <h2>
-        <Link href="/">Home</Link>
+        <Link prefetch href="/">
+          Home
+        </Link>
       </h2>
       <h2>
-        <Link href="/contact">Contact</Link>
+        <Link prefetch href="/contact">
+          Contact
+        </Link>
       </h2>
     </nav>
   );
