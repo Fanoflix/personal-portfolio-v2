c2f57f4e82955e200168663335762f343439c6ae
fanoflix <majidammar428@gmail.com>
2024-10-15 16:58:58 +0500

    DEV: V0 of SiteNavBar component

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -1,10 +1,12 @@
 import { PropsWithChildren } from "react";
+import SiteNavbar from "./SiteNavBar";
 import TopNavbar from "./TopNavbar";
 
 export default function AppShell({ children }: PropsWithChildren) {
   return (
-    <div className="p-6 flex flex-col">
+    <div className="p-6 flex flex-col items-center">
       <TopNavbar />
+      <SiteNavbar />
       {children}
     </div>
   );
--- src/components/SiteNavBar.tsx
+++ src/components/SiteNavBar.tsx
@@ -1,39 +1,15 @@
 "use client";
-import { useTheme } from "next-themes";
-import Image from "next/image";
-export default function Navbar() {
-  const { setTheme } = useTheme();
+import Link from "next/link";
 
+export default function SiteNavbar() {
   return (
-    <nav className="flex items-end justify-end">
-      <div className="flex items-center gap-2">
-        <Image src="/icons/wow100x100.png" alt="Logo" width={20} height={20} />
-        <h1 className="text-2xl font-bold">Ammar</h1>
-        <button
-          onClick={() => {
-            setTheme("dark");
-          }}
-          className="bg-blue-500 text-white px-4 py-2 rounded-md"
-        >
-          Dark
-        </button>
-        <button
-          onClick={() => {
-            setTheme("light");
-          }}
-          className="bg-blue-500 text-white px-4 py-2 rounded-md"
-        >
-          Light
-        </button>
-        <button
-          onClick={() => {
-            setTheme("system");
-          }}
-          className="bg-blue-500 text-white px-4 py-2 rounded-md"
-        >
-          System
-        </button>
-      </div>
+    <nav className="flex items-end gap-5 justify-end">
+      <h2>
+        <Link href="/">Home</Link>
+      </h2>
+      <h2>
+        <Link href="/contact">Contact</Link>
+      </h2>
     </nav>
   );
 }
