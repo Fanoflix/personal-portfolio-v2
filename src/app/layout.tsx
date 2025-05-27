f227f8ab3befac716da9169a70cb3d383d5f27da
fanoflix <majidammar428@gmail.com>
2025-05-27 10:32:42 +0500

    Added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import "./globals.css";
 
 import { Metadata } from "next";
@@ -33,6 +35,7 @@ export default function RootLayout({
           <AppShell>{children}</AppShell>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
