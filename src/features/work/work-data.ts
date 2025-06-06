45399a7b1e78ef934a717f2c41b13a08ebdc6b9f
fanoflix <majidammar428@gmail.com>
2025-06-06 10:16:14 +0500

    DEV: Added link to work data

--- src/features/work/work-data.ts
+++ src/features/work/work-data.ts
@@ -7,6 +7,7 @@ export const WORK_DATA: WorkByYear = {
         name: "Portfolio Website V2",
         label: "sideProject",
         tags: [{ name: "NextJS" }, { name: "Typescript" }, { name: "ShadCN" }],
+        link: "https://github.com/Fanoflix/personal-portfolio-v2",
         company: {
           companyName: "Metal",
         },
@@ -137,7 +138,6 @@ export const WORK_DATA: WorkByYear = {
     {
       project: {
         name: "Analytics Dashboard for In-browser Live-Streaming Service",
-        link: "https://zeacon.com/",
         label: "majorContribution",
         tags: [
           { name: "NextJS" },
@@ -149,6 +149,7 @@ export const WORK_DATA: WorkByYear = {
         company: {
           companyName: "10Pearls",
         },
+        link: "https://zeacon.com/",
       },
       date: "November 1, 2022",
     },
@@ -157,6 +158,7 @@ export const WORK_DATA: WorkByYear = {
         name: "Fatefy — Vue3 Public Template",
         label: "sideProject",
         tags: [{ name: "Vue3" }, { name: "Typescript" }, { name: "Scss" }],
+        link: "https://github.com/Fanoflix/fatefy",
       },
       date: "April 1, 2022",
     },
@@ -171,6 +173,7 @@ export const WORK_DATA: WorkByYear = {
           { name: "AnimeJS" },
           { name: "Google Apps Script" },
         ],
+        link: "https://github.com/Fanoflix/personal-portfolio",
       },
       date: "August 1, 2022",
     },
@@ -193,191 +196,3 @@ export const WORK_DATA: WorkByYear = {
     },
   ],
 };
-
-// export const WORK_DATA: WorkByYear = {
-//   "2025": [
-//     {
-//       project: {
-//         name: "Portfolio Website V2",
-//         label: "sideProject",
-//         tags: [
-//           { name: "NextJS" },
-//           { name: "Typescript" },
-//           { name: "ShadCN" },
-//           { name: "NestJS" },
-//           { name: "Postgres" },
-//           { name: "Redis" },
-//         ],
-//         company: {
-//           companyName: "Metal",
-//         },
-//       },
-//       date: "May 25, 2025",
-//     },
-//     // {
-//     //   project: {
-//     //     name: "Rankings Engine for recommending Investors",
-//     //     label: "majorContribution",
-//     //     tags: [
-//     //       { name: "NextJS" },
-//     //       { name: "Typescript" },
-//     //       { name: "ShadCN" },
-//     //       { name: "NestJS" },
-//     //       { name: "Postgres" },
-//     //       { name: "Redis" },
-//     //     ],
-//     //     company: {
-//     //       companyName: "Metal",
-//     //     },
-//     //   },
-//     //   date: "April 1, 2025",
-//     // },
-//     // {
-//     //   project: {
-//     //     name: "Signup Flow revamp V3",
-//     //     label: "majorContribution",
-//     //     tags: [
-//     //       { name: "NextJS" },
-//     //       { name: "Typescript" },
-//     //       { name: "ShadCN" },
-//     //       { name: "Framer Motion" },
-//     //     ],
-//     //     company: {
-//     //       companyName: "Metal",
-//     //     },
-//     //     link: "https://app.metal.so/signup",
-//     //   },
-//     //   date: "February 1, 2025",
-//     // },
-//   ],
-//   // "2024": [
-//   //   {
-//   //     project: {
-//   //       name: "Major Refactor to DataTable component (Take 2)",
-//   //       label: "minorContribution",
-//   //       tags: [{ name: "NextJS" }, { name: "Typescript" }, { name: "ShadCN" }],
-//   //       company: {
-//   //         companyName: "Metal",
-//   //       },
-//   //     },
-//   //     date: "December 1, 2024",
-//   //   },
-//   //   {
-//   //     project: {
-//   //       name: "Started using Cursor",
-//   //       label: "normal",
-//   //       tags: [{ name: "Cursor" }],
-//   //     },
-//   //     date: "March 1, 2024",
-//   //   },
-//   //   {
-//   //     project: {
-//   //       name: "Signup Flow revamp V1",
-//   //       label: "majorContribution",
-//   //       tags: [{ name: "NextJS" }, { name: "Typescript" }, { name: "ShadCN" }],
-//   //       company: {
-//   //         companyName: "Metal",
-//   //       },
-//   //     },
-//   //     date: "March 1, 2024",
-//   //   },
-//   // ],
-//   // "2023": [
-//   //   {
-//   //     project: {
-//   //       name: "Major Refactor to DataTable component (Take 1)",
-//   //       label: "minorContribution",
-//   //       tags: [{ name: "NextJS" }, { name: "Typescript" }, { name: "ShadCN" }],
-//   //       company: {
-//   //         companyName: "Metal",
-//   //       },
-//   //     },
-//   //     date: "December 1, 2023",
-//   //   },
-//   //   {
-//   //     project: {
-//   //       name: "Feature: Base Pipeline for Investors",
-//   //       label: "majorContribution",
-//   //       tags: [{ name: "NextJS" }, { name: "Typescript" }, { name: "ShadCN" }],
-//   //       company: {
-//   //         companyName: "Metal",
-//   //       },
-//   //     },
-//   //     date: "October 1, 2023",
-//   //   },
-//   //   {
-//   //     project: {
-//   //       name: "Drag and drop on Canvas",
-//   //       label: "majorContribution",
-//   //       tags: [
-//   //         { name: "NextJS" },
-//   //         { name: "Typescript" },
-//   //         { name: "InteractJS" },
-//   //         { name: "Custom Framework for DnD" },
-//   //       ],
-//   //       company: {
-//   //         companyName: "10Pearls",
-//   //       },
-//   //     },
-//   //     date: "March 1, 2023",
-//   //   },
-//   // ],
-//   // "2022": [
-//   //   {
-//   //     project: {
-//   //       name: "Analytics Dashboard for In-browser Live Streaming Service",
-//   //       label: "majorContribution",
-//   //       tags: [
-//   //         { name: "NextJS" },
-//   //         { name: "Typescript" },
-//   //         { name: "Material" },
-//   //         { name: "NewRelic" },
-//   //         { name: "FingerprintJS" },
-//   //       ],
-//   //       company: {
-//   //         companyName: "10Pearls",
-//   //       },
-//   //     },
-//   //     date: "November 1, 2022",
-//   //   },
-//   //   {
-//   //     project: {
-//   //       name: "Fatefy — Vue3 Public Template",
-//   //       label: "sideProject",
-//   //       tags: [{ name: "Vue3" }, { name: "Typescript" }, { name: "Scss" }],
-//   //     },
-//   //     date: "April 1, 2022",
-//   //   },
-//   //   {
-//   //     project: {
-//   //       name: "Portfolio Website V1",
-//   //       label: "sideProject",
-//   //       tags: [
-//   //         { name: "VueJS 3" },
-//   //         { name: "Typescript" },
-//   //         { name: "SCSS" },
-//   //         { name: "AnimeJS" },
-//   //         { name: "Google Apps Script" },
-//   //       ],
-//   //     },
-//   //     date: "August 1, 2022",
-//   //   },
-//   //   {
-//   //     project: {
-//   //       name: "Parking Assistant App — Final Year Project",
-//   //       label: "majorContribution",
-//   //       tags: [
-//   //         { name: "Ionic" },
-//   //         { name: "Typescript" },
-//   //         { name: "NodeJS" },
-//   //         { name: "MongoDB" },
-//   //         { name: "ExpressJS" },
-//   //       ],
-//   //       company: {
-//   //         companyName: "University",
-//   //       },
-//   //     },
-//   //     date: "October 1, 2022",
-//   //   },
-//   // ],
-// };
